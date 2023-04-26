85514746f2ade51489cdccbac16328e4ad69dba7
Simon <simon.michael.te15d@gmail.com>
2023-04-26 22:58:46 +0200

    Started working on Bio

---
+++ src/components/Bio.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+export default function Bio() {
+    return (
+        <div className="flex justify-center">
+            <div className='flex flex-col items-center p-4 gap-4 sm:flex-row w-full max-w-5xl '>
+                <img src="bio_picture.jpg" alt="" className='w-6/12' />
+                <div className='text-center'>
+                    <h1 className='text-2xl font-semibold md:text-4xl'>
+                        Hi, my name is Simon and I'm a Fullstack Developer
+                    </h1>
+                    <p className='text-xs'> I love to create solutions for others and to</p>
+                </div>
+            </div>
+        </div>
+    )
+}
