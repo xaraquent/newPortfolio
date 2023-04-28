84a29e034baf73c592f0c6a93e0fe8a52fb549ce
Simon <simon.michael.te15d@gmail.com>
2023-04-28 22:16:15 +0200

    Added text bio and styling

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -7,10 +7,10 @@ export default function Bio() {
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-8 py-4'>
                 <img src="bio_picture.jpg" alt="" className='rounded-3xl' />
                 <div className='md:col-span-2 flex flex-col justify-center bg-purple-900/40 rounded-3xl p-4'>
-                    <h1 className='text-xl sm:text-3xl md:text-4xl font-semibold md:text-left'>
-                        Hi, my name is Simon and I'm a Fullstack Developer
+                    <h1 className='text-xl text-center sm:text-3xl md:text-4xl font-semibold sm:text-left'>
+                        Hi, I'm Simon, a Fullstack Developer
                     </h1>
-                    <p className='text-xs md:text-left mt-2'> I love to create solutions for others and to</p>
+                    <p className='text-sm text-center sm:text-left mt-2 text-slate-300'> I love to create solutions for others and to see my ideas come to life. I solve new challenges best through collaboration and good communication, and I live by the saying "teamwork makes the dream work".</p>
                 </div>
             </div>
         </div>
