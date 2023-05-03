8193428bcde475f220020f119aaca9ec7b0d1a0f
Simon <simon.michael.te15d@gmail.com>
2023-05-03 15:03:50 +0200

    Added automated swiping and hover icon

--- src/components/SkillSlider.tsx
+++ src/components/SkillSlider.tsx
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect } from 'react';
 import 'keen-slider/keen-slider.min.css';
 import { useKeenSlider } from 'keen-slider/react';
+import SwipeTwoToneIcon from "@mui/icons-material/SwipeTwoTone";
+
 
 interface Icon {
     src: string;
@@ -31,13 +33,48 @@ export default function SkillSlider(): JSX.Element {
         slides: {
             perView: () => Math.ceil(elRef.current?.clientWidth! / 190),
         },
-    });
+    },
+        [
+            (slider) => {
+                let timeout: ReturnType<typeof setTimeout>
+                let mouseOver = false
+                function clearNextTimeout() {
+                    clearTimeout(timeout)
+                }
+                function nextTimeout() {
+                    clearTimeout(timeout)
+                    if (mouseOver) return
+                    timeout = setTimeout(() => {
+                        slider.next()
+                    }, 2000)
+                }
+                slider.on("created", () => {
+                    slider.container.addEventListener("mouseover", () => {
+                        mouseOver = true
+                        clearNextTimeout()
+                    })
+                    slider.container.addEventListener("mouseout", () => {
+                        mouseOver = false
+                        nextTimeout()
+                    })
+                    nextTimeout()
+                })
+                slider.on("dragStarted", clearNextTimeout)
+                slider.on("animationEnded", nextTimeout)
+                slider.on("updated", nextTimeout)
+            },
+        ]
+    );
 
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-8 py-4 max-w-5xl mx-auto'>
             <div className='sm:col-span-2 md:col-span-3 bg-purple-900/40 rounded-3xl p-4' ref={elRef}>
                 <h1 className='text-xl text-center sm:text-3xl md:text-4xl font-semibold mb-4'>Skills</h1>
                 <div ref={ref} className="keen-slider">
+                    <div className=' absolute flex flex-col gap-2 items-center justify-center z-10 h-full w-full duration-200 opacity-0 hover:opacity-100 transition-opacity hover:animate-fade  hover:animate-once  hover:animate-ease-in-out  hover:animate-reverse hover:animate-duration-[1000ms] hover:animate-delay-[1000ms]'>
+                        <p className='text-xl'>Swipe left or right</p>
+                        <SwipeTwoToneIcon fontSize='large' />
+                    </div>
                     {icons.map((icon: Icon, index: number) => (
                         <div key={index} className="keen-slider__slide h-24 flex flex-col gap-2">
                             <img src={icon.src} alt={icon.alt} className="flex-1 h-14 object-contain" />
