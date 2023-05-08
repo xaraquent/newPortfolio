4ee333b440e963bedff1a43edb422408a8c254cb
Simon <simon.michael.te15d@gmail.com>
2023-05-08 14:51:14 +0200

    quick fix to change navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,15 +32,17 @@ export default function Navbar() {
         <div className={`${bgClass} ${stickyClass} z-50 flex justify-center transition-all duration-1000 ease-in-out`}>
             <DropdownMenu />
             <div className="hidden sm:block sm:flex justify-between px-8 py-4 items-center w-full max-w-5xl">
-                <div className="space-x-8 flex">
+                <div className='space-x-3'>
+                    <a href='https://github.com/xaraquent' target='_blank'><GitHubIcon /></a>
+                    <a href='https://www.linkedin.com/in/simon-michael-042b23162/' target='_blank'><LinkedInIcon /></a>
+                </div>
+                <div className="space-x-24 flex justify-center text-lg ">
                     <a className="underline" href="#">Home</a>
                     <a className="underline" href="#skills">Skills</a>
                     <a className="underline" href="#projects">Projects</a>
                     <a className="underline" href="#contact">Contact</a>
                 </div>
-                <div className="flex space-x-3">
-                    <a href='https://github.com/xaraquent' target='_blank'><GitHubIcon /></a>
-                    <a href='https://www.linkedin.com/in/simon-michael-042b23162/' target='_blank'><LinkedInIcon /></a>
+                <div className='empty-content centers-the-nav-buttons'>
                 </div>
             </div>
         </div>
